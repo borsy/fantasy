ed714007d61397f925385d47f60e2432989af1fc
borsy <test@test.com>
2024-05-23 14:29:12 +0200

    ...

--- fantasy/views.py
+++ fantasy/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import ItemSerializer
+from .models import Item
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def getAllItems(request):
+    items = Item.objects.all()
+    ser = ItemSerializer(items, many = True)
