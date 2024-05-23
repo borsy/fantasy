1d20f351877586b897e814182dae7203b92b8bc0
borsy <test@test.com>
2024-05-23 14:13:00 +0200

    ...

--- fantasy/models.py
+++ fantasy/models.py
@@ -7,4 +7,12 @@ class ItemType (models.Model):
         return self.name
         
 
-#class Item(models.Model):
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="static/images/")
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
