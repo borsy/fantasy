0d7f1fb23d3d3b6ee1a4dbe5c75253d6540ffe8d
borsy <test@test.com>
2024-05-23 14:37:36 +0200

    ...

--- fantasy/serializers.py
+++ fantasy/serializers.py
@@ -4,4 +4,4 @@ from .models import Item
 class ItemSerializer(ModelSerializer):
     class Meta:
         model = Item
-        field = '__all__'
+        fields = '__all__'
--- fantasy/views.py
+++ fantasy/views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
-from .serializers import ItemSerializer
 from .models import Item
+from .serializers import ItemSerializer
 from rest_framework.response import Response
 
 @api_view(['GET'])
 def getAllItems(request):
     items = Item.objects.all()
     ser = ItemSerializer(items, many = True)
+    return Response(ser.data)
